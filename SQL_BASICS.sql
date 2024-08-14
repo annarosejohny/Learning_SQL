fb86adc7cfec1f1d7e07669f8da487da27385f10
Anna Rose Johny <55184065+annarosejohny@users.noreply.github.com>
2024-08-14 15:17:23 +0200

    Update SQL_BASICS.sql

--- SQL_BASICS.sql
+++ SQL_BASICS.sql
@@ -1,3 +1,9 @@
 /** Grocery list:
-Bananas(4), Peanut butter (!), chocolate(2)**/
+Bananas(4), Peanut butter (1), chocolate(2)**/
 
+CREATE TABLE groceries(id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER);
+
+INSERT INTO groceries VALUES(1, "Bananas",4);
+INSERT INTO groceries VALUES(2, "Peanut butter",1);
+INSERT INTO groceries VALUES(3, "Chocolate",2);
+SELECT * FROM groceries;
