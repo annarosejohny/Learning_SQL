27dc5cc501d0707983dbccb2bfae4b0c16fc5f5c
Anna Rose Johny <55184065+annarosejohny@users.noreply.github.com>
2024-08-14 15:24:36 +0200

    Create BOOKS.sql

---
+++ BOOKS.sql
@@ -0,0 +1,6 @@
+# Creating Book list database
+CREATE TABLE books(id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+INSERT INTO books VALUES(1, "my world", 5);
+INSERT INTO books VALUES(2, "Life of pie", 8);
+INSERT INTO books VALUES(3, "My life stories",6);
